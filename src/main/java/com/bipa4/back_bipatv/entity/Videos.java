7907d7d6d6b1891c55fa7c87e9ba7606a9bc7c10
Jeonhaeun <21900646@handong.edu>
2023-09-12 11:21:13 +0900

    Feat: 영상 업로드 백엔드 구현
    mp4 파일을 스토리지(S3)에 저장한 후, url과 작성한 영상 관련 정보들을 데이터베이스에 저장하는 기능을 구현함

--- src/main/java/com/bipa4/back_bipatv/entity/Videos.java
+++ src/main/java/com/bipa4/back_bipatv/entity/Videos.java
@@ -1,36 +1,63 @@
 package com.bipa4.back_bipatv.entity;
 
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Builder;
 import lombok.Data;
-
-import javax.persistence.*;
-import java.sql.Timestamp;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Data
-@Table(name="Videos")
+@Table(name = "Videos")
 public class Videos {
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    @Column(name="video_id")
-    private int videoId;
-    @Column(name="video_url",nullable = true, length = 200)
-    private String videoUrl;
-    @Column(name="title",nullable = true, length = 100)
-    private String title;
-    @Column(name="content",nullable = true, length = 400)
-    private String content;
-    @Column(name="read_cnt",nullable = true)
-    private int readCnt;
-    @Column(name="create_at",nullable = true)
-    private Timestamp createAt;
-    @Column(name="private_type")
-    private boolean privateType;
-    @Column(name="comment_permission")
-    private boolean commentPermission;
-    @Column(name="thumbnail",nullable = true, length = 200)
-    private String thumbnail;
-    @Column(name="channel_id")
-    private int channelId;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "video_id")
+  private Long videoId;
 
+  @Column(name = "video_url", nullable = true, length = 200)
+  private String videoUrl;
+
+  @Column(name = "title", nullable = true, length = 100)
+  private String title;
+
+  @Column(name = "content", nullable = true, length = 400)
+  private String content;
+
+  @Column(name = "read_cnt", nullable = true)
+  private int readCnt;
+
+  @CreationTimestamp
+  @Column(name = "create_at")
+  private LocalDateTime createAt = LocalDateTime.now();
+
+  @Column(name = "private_type")
+  private boolean privateType;
+
+  @Column(name = "comment_permission")
+  private boolean commentPermission;
+
+  @Column(name = "thumbnail", nullable = true, length = 200)
+  private String thumbnail;
+
+  @Column(name = "channel_id")
+  private int channelId;
+
+  @Builder
+  public Videos(String videoUrl, String title, String content, boolean privateType,
+      boolean commentPermission, String thumbnail, int channelId) {
+    this.videoUrl = videoUrl;
+    this.title = title;
+    this.content = content;
+    this.privateType = privateType;
+    this.commentPermission = commentPermission;
+    this.thumbnail = thumbnail;
+    this.channelId = channelId;
+  }
 }
