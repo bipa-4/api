d650bf2de2dd73a7b1f8e11197c0da31810e148f
Jeonhaeun <21900646@handong.edu>
2023-09-13 23:30:15 +0900

    Feat: 영상 조회 백엔드 구현
    최신 순으로 전체 영상 조회, 카테고리별 영상 조회, 조회수 급상승 영상 조회 구현

--- src/main/java/com/bipa4/back_bipatv/repository/VideoRepository.java
+++ src/main/java/com/bipa4/back_bipatv/repository/VideoRepository.java
@@ -1,8 +1,10 @@
-package com.bipa4.back_bipatv.dao;
+package com.bipa4.back_bipatv.repository;
 
 import com.bipa4.back_bipatv.entity.Videos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VideoRepository extends JpaRepository<Videos, Long> {
 
 }
