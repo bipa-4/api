fafe125011a424e4ea69dc0ddca8da4de4b030b5
DESKTOP-TALH15P\금정산2_PC2 <gud0775@naver.com>
2023-10-04 10:04:40 +0900

    feat:Cors에러 테스트

--- src/main/java/com/bipa4/back_bipatv/filter/CorsFilter.java
+++ src/main/java/com/bipa4/back_bipatv/filter/CorsFilter.java
@@ -1,39 +1,45 @@
-//package com.bipa4.back_bipatv.filter;
-//
-//import java.io.IOException;
-//import javax.servlet.Filter;
-//import javax.servlet.FilterChain;
-//import javax.servlet.ServletException;
-//import javax.servlet.ServletRequest;
-//import javax.servlet.ServletResponse;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import org.springframework.core.Ordered;
-//import org.springframework.core.annotation.Order;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//@Order(Ordered.HIGHEST_PRECEDENCE)
-//public class CorsFilter implements Filter {
-//
-//  @Override
-//  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-//      throws IOException, ServletException {
-//    HttpServletRequest request = (HttpServletRequest) req;
-//    HttpServletResponse response = (HttpServletResponse) res;
-//
-////    response.setHeader("Access-Control-Allow-Origin", "https://bipa-streamwave.vercel.app");
-//    response.setHeader("Access-Control-Allow-Origin", "*");
-//    response.setHeader("Access-Control-Allow-Credentials", "true");
-//    response.setHeader("Access-Control-Allow-Methods", "*");
-//    response.setHeader("Access-Control-Max-Age", "3600");
-//    response.setHeader("Access-Control-Allow-Headers",
-//        "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//
-//    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-//      response.setStatus(HttpServletResponse.SC_OK);
-//    } else {
-//      chain.doFilter(req, res);
-//    }
-//  }
-//}
+package com.bipa4.back_bipatv.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+      throws IOException, ServletException {
+    HttpServletRequest request = (HttpServletRequest) req;
+    HttpServletResponse response = (HttpServletResponse) res;
+    String origin = request.getHeader("origin");
+    System.out.println("origin값:" + origin);
+    
+    if (origin.equals("http://localhost:8080") || origin.equals(
+        "https://https://bipa-streamwave.vercel.app")) {
+      response.setHeader("Access-Control-Allow-Origin", origin);
+    }
+//    response.setHeader("Access-Control-Allow-Origin", "https://bipa-streamwave.vercel.app");
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Credentials", "true");
+    response.setHeader("Access-Control-Allow-Methods", "*");
+    response.setHeader("Access-Control-Max-Age", "3600");
+    response.setHeader("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+      response.setStatus(HttpServletResponse.SC_OK);
+    } else {
+      chain.doFilter(req, res);
+    }
+  }
+}
--- src/main/java/com/bipa4/back_bipatv/filter/CorsFilterLocalhost.java
+++ src/main/java/com/bipa4/back_bipatv/filter/CorsFilterLocalhost.java
@@ -1,43 +1,47 @@
-package com.bipa4.back_bipatv.filter;
-
-import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
-
-@Component
-public class CorsFilterLocalhost implements Filter {
-
-  @Override
-  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-      throws IOException, ServletException {
-    HttpServletResponse response = (HttpServletResponse) res;
-    HttpServletRequest request = (HttpServletRequest) req;
-
-    response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    response.setHeader("Access-Control-Allow-Credentials", "true");
-    response.setHeader("Access-Control-Allow-Methods", "*");
-    response.setHeader("Access-Control-Max-Age", "3600");
-    response.setHeader("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-      response.setStatus(HttpServletResponse.SC_OK);
-    } else {
-      chain.doFilter(req, res);
-    }
-  }
-
-  @Override
-  public void init(FilterConfig filterConfig) {
-  }
-
-  @Override
-  public void destroy() {
-  }
-}
+//package com.bipa4.back_bipatv.filter;
+//
+//import java.io.IOException;
+//import javax.servlet.Filter;
+//import javax.servlet.FilterChain;
+//import javax.servlet.FilterConfig;
+//import javax.servlet.ServletException;
+//import javax.servlet.ServletRequest;
+//import javax.servlet.ServletResponse;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class CorsFilterLocalhost implements Filter {
+//
+//  @Override
+//  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+//      throws IOException, ServletException {
+//    HttpServletResponse response = (HttpServletResponse) res;
+//    HttpServletRequest request = (HttpServletRequest) req;
+//    String origin = request.getHeader("origin");
+//    System.out.println("origin값:" + origin);
+//    if (origin.equals("http://localhost:8080") || origin.equals(
+//        "https://https://bipa-streamwave.vercel.app")) {
+//      response.setHeader("Access-Control-Allow-Origin", origin);
+//    }
+//    response.setHeader("Access-Control-Allow-Credentials", "true");
+//    response.setHeader("Access-Control-Allow-Methods", "*");
+//    response.setHeader("Access-Control-Max-Age", "3600");
+//    response.setHeader("Access-Control-Allow-Headers",
+//        "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+//      response.setStatus(HttpServletResponse.SC_OK);
+//    } else {
+//      chain.doFilter(req, res);
+//    }
+//  }
+//
+//  @Override
+//  public void init(FilterConfig filterConfig) {
+//  }
+//
+//  @Override
+//  public void destroy() {
+//  }
+//}
