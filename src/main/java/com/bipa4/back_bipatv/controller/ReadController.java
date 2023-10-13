f3a88a05df3f4ae75c27acd2516a53b038e9028b
DESKTOP-TALH15P\금정산2_PC2 <gud0775@naver.com>
2023-10-13 16:22:28 +0900

    Fix:accessToken detail테스트

--- src/main/java/com/bipa4/back_bipatv/controller/ReadController.java
+++ src/main/java/com/bipa4/back_bipatv/controller/ReadController.java
@@ -97,9 +97,12 @@ public class ReadController {
       @CookieValue(name = "accessToken", required = false) String accessToken) {
     int viewsResult = videoService.plusViews(id); //  조회수 상승
     GetDetailResponseDto video = videoService.getVideoDetail(id);
+    System.out.println("detail AT:" + accessToken);
+
     if (accessToken != null) {
       video.setIsLike(videoService.getFavorite(id, accessToken));// 좋아요 버튼 눌렀는지 여부
     }
+    System.out.println(video.getIsLike());
     return new ResponseEntity<GetDetailResponseDto>(video, HttpStatus.OK);
   }
 
