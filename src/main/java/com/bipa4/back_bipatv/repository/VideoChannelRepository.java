6518095e381c92c5c2b129ae8201b9e8806e8be7
Jeonhaeun <21900646@handong.edu>
2023-10-17 14:33:00 +0900

    Feature: 검색 기능 수정
    UUID byte[] 형식으로 나오는 문제 해결

--- src/main/java/com/bipa4/back_bipatv/repository/VideoChannelRepository.java
+++ src/main/java/com/bipa4/back_bipatv/repository/VideoChannelRepository.java
@@ -14,7 +14,7 @@ public interface VideoChannelRepository extends JpaRepository<Videos, Long> {
 
   @Modifying
   @Query(value =
-      "SELECT video_id as videoId, v.content, create_at as createAt, v.private_type as privateType, read_cnt as readCnt, thumbnail, title as videoTitle, profile_url as channelProfileUrl, name as channelName FROM videos v\n"
+      "SELECT BIN_TO_UUID(video_id) as videoId, v.content, create_at as createAt, v.private_type as privateType, read_cnt as readCnt, thumbnail, title as videoTitle, profile_url as channelProfileUrl, name as channelName FROM videos v\n"
           + "    left outer join channels c on v.channel_id = c.channel_id\n"
           + "    WHERE MATCH (v.title, v.content)\n"
           + "    AGAINST (:searchQuery WITH QUERY EXPANSION)", nativeQuery = true)
