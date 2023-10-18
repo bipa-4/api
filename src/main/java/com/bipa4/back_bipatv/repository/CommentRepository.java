211b9904a1fd2a67b9c23757e8b22a833fe39d52
GongJinYoung <wlsdud3297@naver.com>
2023-10-18 09:39:39 +0900

    댓글 대댓글 조회 기능 수정

--- src/main/java/com/bipa4/back_bipatv/repository/CommentRepository.java
+++ src/main/java/com/bipa4/back_bipatv/repository/CommentRepository.java
@@ -1,5 +1,6 @@
 package com.bipa4.back_bipatv.repository;
 
+import com.bipa4.back_bipatv.dto.comment.ChildCommentResponse;
 import com.bipa4.back_bipatv.dto.comment.CommentResponse;
 import com.bipa4.back_bipatv.entity.Comments;
 import io.lettuce.core.dynamic.annotation.Param;
@@ -13,24 +14,26 @@ public interface CommentRepository extends JpaRepository<Comments, UUID> {
 
 
   //
-  @Query(value = "SELECT accounts.profile_url, accounts.name, comments.content, comments.create_at "
-      +
-      "FROM comments comments " +
-      "JOIN accounts accounts " +
-      "ON comments.account_id = accounts.account_id " +
-      "WHERE video_id = :videoId AND parent_child = 0 " +
-      "ORDER BY comments.create_at", nativeQuery = true)
+  @Query(value = "SELECT channels.profile_url AS channelProfileUrl, channels.name AS channelName, comments.content AS content, comments.create_at AS createAt, INSERT(INSERT(INSERT(INSERT(HEX(comments.comment_id), 9, 0, '-'), 14, 0, '-'), 19, 0, '-'), 24, 0, '-') AS commentId, comments.group_index AS groupIndex, childCount\n" +
+          "FROM (SELECT *, COUNT(*) over (PARTITION BY group_index) -1 AS childCount FROM bipaTV.comments) comments\n" +
+          "LEFT JOIN accounts\n" +
+          "ON comments.account_id = accounts.account_id\n" +
+          "LEFT JOIN channels\n" +
+          "ON accounts.account_id = channels.account_id\n" +
+          "WHERE video_id = :videoId AND parent_child = 0 \n" +
+          "ORDER BY comments.create_at;", nativeQuery = true)
   List<CommentResponse> findParentComments(@Param("videoId") UUID videoId);
 
-  @Query(value = "SELECT accounts.profile_url, accounts.name, comments.content, comments.create_at "
-      +
-      "FROM comments comments " +
-      "JOIN accounts accounts " +
-      "ON comments.account_id = accounts.account_id " +
-      "WHERE video_id = :videoId AND parent_child = 1 AND group_index = :groupIndex " +
-      "ORDER BY comments.create_at", nativeQuery = true)
-  List<CommentResponse> findChildComments(@Param("videoId") UUID videoId,
-      @Param("groupIndex") int groupIndex);
+  @Query(value = "SELECT channels.profile_url AS channelProfileUrl, channels.name AS channelName, comments.content AS content, comments.create_at AS createAt, INSERT(INSERT(INSERT(INSERT(HEX(comments.comment_id), 9, 0, '-'), 14, 0, '-'), 19, 0, '-'), 24, 0, '-') AS commentId, comments.group_index AS groupIndex\n" +
+          "FROM comments \n" +
+          "LEFT JOIN accounts\n" +
+          "ON comments.account_id = accounts.account_id\n" +
+          "LEFT JOIN channels\n" +
+          "ON accounts.account_id = channels.account_id\n" +
+          "WHERE video_id = :videoId AND parent_child = 1 AND group_index = :groupIndex\n" +
+          "ORDER BY comments.create_at", nativeQuery = true)
+  List<ChildCommentResponse> findChildComments(@Param("videoId") UUID videoId,
+                                               @Param("groupIndex") int groupIndex);
 
 
 }
