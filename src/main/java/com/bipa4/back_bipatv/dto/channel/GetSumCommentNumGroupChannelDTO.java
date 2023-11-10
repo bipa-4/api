e77ea6227e2c28e08d66298c4b2800b01746aea3
Jeonhaeun <21900646@handong.edu>
2023-11-10 15:51:48 +0900

    Fix: PR 충돌 재수정

--- src/main/java/com/bipa4/back_bipatv/dto/channel/GetSumCommentNumGroupChannelDTO.java
+++ src/main/java/com/bipa4/back_bipatv/dto/channel/GetSumCommentNumGroupChannelDTO.java
@@ -1,14 +1,13 @@
 package com.bipa4.back_bipatv.dto.channel;
 
+import java.util.UUID;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.UUID;
 
 @NoArgsConstructor
 @Data
 public class GetSumCommentNumGroupChannelDTO {
 
-    private UUID channelId;
-    private Integer sumCommentNum;//댓글 총 합
+  private UUID channelId;
+  private Integer sumCommentNum;//댓글 총 합
 }
