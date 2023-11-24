0141a02c0a33ef5124d23222535035e1bfe4ce91
DESKTOP-TALH15P\금정산2_PC2 <gud0775@naver.com>
2023-11-24 16:42:48 +0900

    fix: redis비밀번호 추가

--- src/main/java/com/bipa4/back_bipatv/config/RedisConfig.java
+++ src/main/java/com/bipa4/back_bipatv/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -18,16 +19,24 @@ public class RedisConfig {
 
   private final String redisHost;
   private final int redisPort;
+  private final String password;
 
-  public RedisConfig(@Value("${spring.redis.host}") final String redisHost,
-      @Value("${spring.redis.port}") final int redisPort) {
+  public RedisConfig(
+      @Value("${spring.redis.host}") final String redisHost,
+      @Value("${spring.redis.port}") final int redisPort,
+      @Value("${spring.redis.password}") final String password) {
     this.redisHost = redisHost;
     this.redisPort = redisPort;
+    this.password = password;
   }
 
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory(redisHost, redisPort);
+    RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
+    redisConfiguration.setHostName(redisHost);
+    redisConfiguration.setPort(redisPort);
+    redisConfiguration.setPassword(password);
+    return new LettuceConnectionFactory(redisConfiguration);
   }
 
   @Bean
