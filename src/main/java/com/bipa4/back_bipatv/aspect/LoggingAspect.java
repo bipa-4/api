b3cc0f6f29035770d4878b59abbf1414d79bd96e
DESKTOP-TALH15P\금정산2_PC2 <gud0775@naver.com>
2023-11-27 17:15:21 +0900

    fix: log 수정

--- src/main/java/com/bipa4/back_bipatv/aspect/LoggingAspect.java
+++ src/main/java/com/bipa4/back_bipatv/aspect/LoggingAspect.java
@@ -4,7 +4,7 @@ import com.bipa4.back_bipatv.service.LogService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -19,37 +19,48 @@ public class LoggingAspect {
   private final LogService logService;
 
 
-  @AfterReturning("@annotation(org.springframework.web.bind.annotation.PutMapping)")
+  @After("@annotation(org.springframework.web.bind.annotation.PutMapping)")
   public void putLogAfterRequest() {
     // 요청을 처리한 후 로깅
     // 예: 로깅 메서드를 호출하여 응답 로그 기록
-
-    HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-    HttpServletResponse httpServletResponse = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
-    logService.saveLog(request, httpServletResponse, "수정");
+    try {
+      // 요청을 처리한 후 로깅
+      // 예: 로깅 메서드를 호출하여 응답 로그 기록
+      HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+      HttpServletResponse httpServletResponse = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
+      logService.saveLog(request, httpServletResponse, "수정");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
-  @AfterReturning("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
+  @After("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
   public void deleteLogAfterRequest() {
     // 요청을 처리한 후 로깅
     // 예: 로깅 메서드를 호출하여 응답 로그 기록
-
-    HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-    HttpServletResponse httpServletResponse = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
-    logService.saveLog(request, httpServletResponse, "삭제");
-
+    try {
+      HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+      HttpServletResponse httpServletResponse = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
+      logService.saveLog(request, httpServletResponse, "삭제");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
-  @AfterReturning("@annotation(org.springframework.web.bind.annotation.PostMapping)")
+  @After("@annotation(org.springframework.web.bind.annotation.PostMapping)")
   public void insertLogAfterRequest() {
     // 요청을 처리한 후 로깅
     // 예: 로깅 메서드를 호출하여 응답 로그 기록
-
-    HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-    HttpServletResponse httpServletResponse = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
-    if (!request.getRequestURI().contains("logout")) {
-      logService.saveLog(request, httpServletResponse, "요청");
+    try {
+      HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+      HttpServletResponse httpServletResponse = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
+      if (!request.getRequestURI().contains("logout")) {
+        logService.saveLog(request, httpServletResponse, "요청");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+
 
   }
 }
